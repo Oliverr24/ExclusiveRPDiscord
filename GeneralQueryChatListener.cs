cbb1940d94da25ab5199209d7fe1e595f46b4c2d
Oliverr24 <47751515+Oliverr24@users.noreply.github.com>
2021-06-13 21:20:26 +0100

    Add files via upload

---
+++ GeneralQueryChatListener.cs
@@ -0,0 +1,76 @@
+﻿using DSharpPlus;
+using DSharpPlus.Entities;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExclusiveRPBot.Methods {
+    public class GeneralQueryChatListener {
+
+        public async Task GeneralQueryListenToChat(DiscordClient client, DiscordGuild guild, DiscordMessage message) {
+
+            if (client == null || guild == null || message == null) {
+                return;
+            }
+
+            if (guild.Id != 810465971201376256 && guild.Id != 827523101981540362) {
+                return;
+            }
+
+            var messageMember = (DiscordMember) message.Author;
+
+            var respondChannel = message.Channel;
+            var ticketChannel = guild.Channels.FirstOrDefault(x => x.Value.Name.ToLower().Contains("open-a-ticket")).Value;
+
+            var replyEmbed = new DiscordEmbedBuilder() {
+                Color = DiscordColor.Cyan,
+                Timestamp = DateTime.UtcNow,
+            };
+
+            string[] sentences = message.Content.Split('.', '!', '?');
+
+            foreach(var sen in sentences) {
+
+                //Ban query
+                if (sen.ToLower().Contains("i am banned") || sen.ToLower().Contains("help i'm banned") || sen.ToLower().Contains("help i got banned") || sen.ToLower().Contains("help im banned") || sen.ToLower().Contains("why am i banned") || sen.ToLower().Contains("can i be unbanned") || ((sen.ToLower().Contains("explosion") || sen.ToLower().Contains("explosion")) && (sen.ToLower().Contains("got banned") || sen.ToLower().Contains("banned")))) {
+
+                    replyEmbed.Title = "Help I Am Banned";
+                    replyEmbed.Description = $"{messageMember.Mention} please {ticketChannel.Mention}. This will be looked at as soon as possble. Please do not ping any staff members regarding this.";
+
+                    await respondChannel.SendMessageAsync(embed: replyEmbed);
+
+                    return;
+
+                }
+
+                //Map or Texture query
+                if (sen.ToLower().Contains("map not loading") || sen.ToLower().Contains("texture bug") || sen.ToLower().Contains("invisible roads") || sen.ToLower().Contains("bugged textures")) {
+                    
+                    replyEmbed.Title = "Texture Issues In Game";
+                    replyEmbed.Description = $"{messageMember.Mention} please make sure your settings are as follows or lower: Graphics **all** set to 'Normal' or lower. Also check 'Population Density', 'Population Variety', and 'Distance Scaling'. If they're maxed out, lower them down slightly. Further issues, please {ticketChannel.Mention}";
+
+                    await respondChannel.SendMessageAsync(embed: replyEmbed);
+
+                    return;
+
+                }
+
+                //Error queries
+                if (sen.ToLower().Contains("curl error") || sen.ToLower().Contains("curl error 18") || sen.ToLower().Contains("curl error 28") || sen.ToLower().Contains("curl 18 error") || sen.ToLower().Contains("curl 28 error")) {
+
+                    replyEmbed.Title = "Curl Error";
+                    replyEmbed.Description = $"{messageMember.Mention} please do the following and try again.. Close down FiveM and Steam. Re-open Steam, and then open FiveM and try connect again. \nIf that fails, try connecting through **'F8'** and typing `connect cfxe.re/join/kpxpqa`.";
+
+                    await respondChannel.SendMessageAsync(embed: replyEmbed);
+
+                    return;
+
+                }
+
+            }
+
+
+        }
+
+    }
+}
---
+++ Report.cs
@@ -0,0 +1,98 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using DSharpPlus.Interactivity.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExclusiveRPBot.Commands {
+    public class Report : BaseCommandModule {
+
+        [Command("report")]
+        [Hidden]
+        public async Task ReportCommand(CommandContext ctx) {
+
+            if (ctx.Guild == null) {
+                return;
+            }
+
+            if (ctx.Guild.Id != 827523101981540362) {
+                return;
+            }
+
+            await ctx.Message.DeleteAsync();
+
+            var reportChannel = ctx.Guild.Channels.FirstOrDefault(x => x.Value.Name.Contains("reports")).Value;
+
+            var interactivity = ctx.Client.GetInteractivity();
+
+            //
+            //Patient
+            //Incident
+            //Injury
+            //Solution
+            //
+
+            List<DiscordMessage> delMsg = new List<DiscordMessage>();
+
+            //Patient Question 
+            var patientMessage = await ctx.Channel.SendMessageAsync("Please type out the **Patients** name.");
+
+            delMsg.Add(patientMessage);
+
+            var patientResponse = await interactivity.WaitForMessageAsync(x => x.Author.Id == ctx.Member.Id && x.Channel == ctx.Channel);
+
+            delMsg.Add(patientResponse.Result);
+
+            //Incident Question
+            var incidentMessage = await ctx.Channel.SendMessageAsync("Please type out the **Incident** that occured.");
+
+            delMsg.Add(incidentMessage);
+
+            var incidentResponse = await interactivity.WaitForMessageAsync(x => x.Author.Id == ctx.Member.Id && x.Channel == ctx.Channel);
+
+            delMsg.Add(incidentResponse.Result);
+
+            //Injury Question
+            var injuryMessage = await ctx.Channel.SendMessageAsync("Please type out the **Injury** the patient sustained.");
+
+            delMsg.Add(injuryMessage);
+
+            var injuryResponse = await interactivity.WaitForMessageAsync(x => x.Author.Id == ctx.Member.Id && x.Channel == ctx.Channel);
+
+            delMsg.Add(injuryResponse.Result);
+
+            //Solution Question
+            var solutionMessage = await ctx.Channel.SendMessageAsync("Please type out the **Soltuion** for the injury at hand.");
+
+            delMsg.Add(solutionMessage);
+
+            var solutionResponse = await interactivity.WaitForMessageAsync(x => x.Author.Id == ctx.Member.Id && x.Channel == ctx.Channel);
+
+            delMsg.Add(solutionResponse.Result);
+
+            var reportEmbed = new DiscordEmbedBuilder() {
+                Title = "EMS Report",
+                Color = DiscordColor.Cyan,
+                Timestamp = DateTime.UtcNow,
+                Footer = new DiscordEmbedBuilder.EmbedFooter {
+                    Text = $"Submitted by: {ctx.Member.DisplayName}",
+                    IconUrl = $"{ctx.Member.AvatarUrl}"
+                }
+            };
+
+            reportEmbed.AddField("Patient:", $"{patientResponse.Result.Content}");
+            reportEmbed.AddField("Incident:", $"{incidentResponse.Result.Content}");
+            reportEmbed.AddField("Injury:", $"{injuryResponse.Result.Content}");
+            reportEmbed.AddField("Solution:", $"{solutionResponse.Result.Content}");
+
+            await reportChannel.SendMessageAsync(embed: reportEmbed);
+
+            await ctx.Channel.DeleteMessagesAsync(delMsg);
+
+        }
+
+    }
+}
